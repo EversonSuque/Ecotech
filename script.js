8ee189b294d2513430f4a16711b199fafb487d67
EVERSON SUQUE <148006969+EversonSuque@users.noreply.github.com>
2024-09-12 16:33:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ document.addEventListener("DOMContentLoaded", () => {
         resultado.textContent = `Ao descartar corretamente ${peso} kg de eletrônicos, você ajudou a economizar aproximadamente ${impacto.toFixed(2)} kg de CO2!`;
     });
 });
+function initMap() {
+    // Localização central do Espírito Santo
+    const esLocation = { lat: -19.1834, lng: -40.3089 };
+
+    // Criando um novo mapa
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: esLocation,
+    });
+
+    // Marcador de exemplo
+    const marker = new google.maps.Marker({
+        position: esLocation,
+        map: map,
+        title: "Espírito Santo",
+    });
+}
